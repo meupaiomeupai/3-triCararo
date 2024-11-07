644068deea442cfbbd72940d3dce6f43747d78d3
meupaiomeupai <111287354+meupaiomeupai@users.noreply.github.com>
2024-11-06 21:35:16 -0300

    informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,20 +1,20 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-maquiagem.json'
 
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesMaquiagem() {
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasUsandoMaquiagem = (dados.total_pessoas_usando / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-    const horas = parseInt(dados.tempo_medio)
-    const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+    const horas = parseInt(dados.tempo_medio_diario)
+    const minutos = Math.round((dados.tempo_medio_diario - horas) * 60)
+    const porcentagemUsandoMaquiagem = ((pessoasUsandoMaquiagem / pessoasNoMundo) * 100).toFixed(2)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `Sabia que no mundo há <span>${pessoasNoMundo} bilhões</span> de pessoas e aproximadamente <span>${pessoasUsandoMaquiagem} bilhões</span> utilizam produtos de maquiagem regularmente? Em média, elas gastam <span>${horas} horas</span> e <span>${minutos} minutos</span> por dia usando produtos de beleza.<br>Isso representa cerca de <span>${porcentagemUsandoMaquiagem}%</span> da população mundial.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+visualizarInformacoesMaquiagem()
